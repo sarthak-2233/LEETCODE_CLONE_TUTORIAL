e4d17a18f89a09dae3d69473da0f2ab429f63779
Sarthak Sharma <sarthak.sharma33333@gmail.com>
2025-11-29 23:32:24 +0530

    Update db.js

--- Day01/src/Config/db.js
+++ Day01/src/Config/db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+// connection for database 
 async function main(){
   await  mongoose.connect(process.env.DB_URL);
   console.log("Database connected successfully");
