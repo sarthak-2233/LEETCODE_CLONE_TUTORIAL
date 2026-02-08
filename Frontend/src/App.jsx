b818376becdeff70097e1317c172cdfcbe9fdaf2
sarthak-2233 <sarthak.sharma33333@gmail.com>
2026-02-09 01:43:53 +0530

    DAY 13 end

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,21 @@ import { Routes, Route } from "react-router";
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import {useSelector,useDispatch} from "react-redux";
+import { checkAuth } from './../authSlice';
+import { useEffect } from 'react';
+
 const App = () => {
+  
+  // isAuthenticated
+  const { isAuthenticated } = useSelector(state => state.auth);
+   const dispatch = useDispatch();
+
+    useEffect(()=>{
+      dispatch(checkAuth());
+    },[dispatch])
+
+
   return (
     <>
      <Routes>
