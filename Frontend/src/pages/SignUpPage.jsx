019ee43d8524f7c3ffe1d91853eb106dc811ec96
sarthak-2233 <sarthak.sharma33333@gmail.com>
2026-02-08 23:22:27 +0530

    zod

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -1,27 +1,37 @@
 import React from 'react'
 import {useEffect,useState} from 'react'
 import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
+
+
+// Schema Validation using Zod
+const userSchema= z.object({
+  firstName:z.string().min(3,"Name Should be at least 3 characters"),
+  emailId:z.string().email("Invalid Email Address"),
+  password:z.string().min(6,"Password should be at least 6 characters")
+})
 
 const SignUpPage = () => {
-
   const {
     register,handleSubmit,formState: { errors },} = useForm();
       
-    // const submittedData= (data)=>{
-    //   console.log(data);
-    // }
+    const submittedData= (data)=>{
+      console.log(data);
+    }
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(submittedData)}>
       <input {...register('firstName')} 
         placeholder='Enter First Name'
       />   
-      <input {...register('email')}
+      <input {...register('emailId')}
         placeholder='Enter Email'
       />
       <input {...register('password')}
         placeholder='Enter Password'
+        type='password'
       />
-      <button type='submit' className='btn'>Sign Up</button>
+      <button type='submit' className='btn btn-lg'>Sign Up</button>
     </form>   
     
   )
